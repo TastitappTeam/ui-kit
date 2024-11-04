914d3d2cb9cd493b7be249c43c769c67aa94bc27
Maxime CARABINA <maxime.carabina@epitech.eu>
2024-11-04 22:36:45 +0100

    remove comments

--- src/components/Badge/Badge.stories.ts
+++ src/components/Badge/Badge.stories.ts
@@ -2,18 +2,13 @@ import type { Meta, StoryObj } from '@storybook/react';
 
 import { Badge } from './Badge';
 
-// More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
 const meta = {
   title: 'Components/Badge',
   component: Badge,
   parameters: {
-    // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/configure/story-layout
     layout: 'centered',
   },
-  // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/writing-docs/autodocs
   tags: ['autodocs'],
-  // More on argTypes: https://storybook.js.org/docs/api/argtypes
-
   argTypes: {
     variant: {
       options: ['flat', 'outline'],
@@ -39,7 +34,6 @@ const meta = {
 export default meta;
 type Story = StoryObj<typeof meta>;
 
-// More on writing stories with args: https://storybook.js.org/docs/writing-stories/args
 export const Default: Story = {
   args: {
     children: 'Badge',
