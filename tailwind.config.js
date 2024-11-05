9003f3bb86b83c0d011b46ccca01f536f0a91e42
Maxime Carabina <72019005+maxime-carabina@users.noreply.github.com>
2024-11-05 11:27:36 +0100

    feat(chromatic): setup chromatic (#10)

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,4 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./src/**/*.{js,ts,jsx,tsx}'],
-  theme: {
-    extend: {
-      fontFamily: {
-        AvenirNext: ['Avenir Next', 'sans-serif'],
-        Noto: ['Noto Sans', 'sans-serif'],
-      },
-      colors: {
-        brand: '#FA7268',
-      },
-    },
-  },
-  plugins: [],
+  presets: [require('./presets/tastit-preset.js')],
 };
