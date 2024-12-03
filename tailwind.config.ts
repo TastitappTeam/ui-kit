ee4580f5da060465fcfaaae27dc0fe0fc7b20a92
Maxime CARABINA <maxime.carabina@epitech.eu>
2024-12-03 15:06:48 +0100

    feat(Card): added component

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,9 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 
 import tastitPreset from './presets/tastit-preset';
+import animate from 'tailwindcss-animate';
 
 const config = {
   presets: [tastitPreset],
+  plugins: [animate],
 };
 
 export default config;
