447b18f920d60dafc9cea3b9ee31833f78961705
Maxime Carabina <72019005+maxime-carabina@users.noreply.github.com>
2025-02-12 14:46:07 +0100

    feat(sheet): added new component (#39)

---
+++ src/components/Sheet/Sheet.stories.tsx
@@ -0,0 +1,59 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import { Button, Label, Input } from '../';
+
+import {
+  Sheet,
+  SheetClose,
+  SheetContent,
+  SheetDescription,
+  SheetFooter,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from './Sheet';
+
+const meta = {
+  title: 'Components/Sheet',
+  component: Sheet,
+  parameters: {
+    layout: 'centered',
+  },
+  tags: ['autodocs'],
+} satisfies Meta<typeof Sheet>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  render: () => (
+    <Sheet>
+      <SheetTrigger asChild>
+        <Button>Open</Button>
+      </SheetTrigger>
+      <SheetContent>
+        <SheetHeader>
+          <SheetTitle>Edit profile</SheetTitle>
+          <SheetDescription>
+            Make changes to your profile here. Click save when you&apos;re done.
+          </SheetDescription>
+        </SheetHeader>
+        <div className="grid gap-4 py-4">
+          <div className="flex flex-col gap-4">
+            <Label htmlFor="name">Name</Label>
+            <Input id="name" value="Pedro Duarte" className="w-full" />
+          </div>
+          <div className="flex flex-col gap-4">
+            <Label htmlFor="username">Username</Label>
+            <Input id="username" value="@peduarte" className="w-full" />
+          </div>
+        </div>
+        <SheetFooter>
+          <SheetClose asChild>
+            <Button type="submit">Save changes</Button>
+          </SheetClose>
+        </SheetFooter>
+      </SheetContent>
+    </Sheet>
+  ),
+};
